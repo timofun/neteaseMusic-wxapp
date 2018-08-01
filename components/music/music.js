3dfa2f81a18fa50cac165645d9acbccdf00f5316
timofun <timo_quiet@163.com>
2018-08-01 22:33:08 +0800

    player页面加了左滑切换下一曲

--- components/music/music.js
+++ components/music/music.js
@@ -20,7 +20,9 @@ Component({
   data: {
     playing: false,
     waittingUrl: 'images/player@waitting.png',
-    playingUrl: 'images/player@playing.png'
+    playingUrl: 'images/player@playing.png',
+    touchDot: 0,
+    position: Number
   },
 
   attached: function() {
@@ -86,7 +88,37 @@ Component({
       mMgr.onEnded(()=>{
         this._recoverPlaying()
       })
+    },
+    onTouchstart: function (e) {
+      console.log(e)
+      const pageX = e.touches[0].pageX;
+      this.setData({
+        touchDot: pageX
+      })
+      console.log('touchDot', this.data.touchDot)
+       // 获取触摸时的原点
+    },
+    onTouchmove: function (e) {
+      console.log(e)
+      let touch = e.changedTouches[0];
+      let touchMove = e.changedTouches[0].pageX;
+      let deltaX = touch.pageX - this.data.touchDot
+      if (deltaX > 0) {
+        return
+      }
+      console.log(deltaX)
+      mMgr.stop()
+      this.setData({
+        playing: false,
+      })
+      this.setData({
+        playing: true,
+      })
+      mMgr.src = this.properties.src
+      mMgr.coverImgUrl = this.properties.img
+      mMgr.title = this.properties.name
+    },
+    onTouchend: function (e) {
     }
-
   }
 })
